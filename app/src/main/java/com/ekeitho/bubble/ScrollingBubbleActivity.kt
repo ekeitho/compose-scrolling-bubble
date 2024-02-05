ca376d74b9362bbad8c56f4a546bd3c0eb3d9109
Keith Abdulla <kabdulla@squareup.com>
2024-02-05 13:45:39 +1100

    update to latest version of compose & demonstrate usage of ImmutableList to reduce recompositions

--- app/src/main/java/com/ekeitho/bubble/ScrollingBubbleActivity.kt
+++ app/src/main/java/com/ekeitho/bubble/ScrollingBubbleActivity.kt
@@ -3,86 +3,100 @@ package com.ekeitho.bubble
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.ekeitho.bubble.domain.Contact
 import com.ekeitho.bubble.ui.theme.ScrollingBubbleTheme
-import com.ekeitho.bubble.views.*
+import com.ekeitho.bubble.views.ContactListWithScroller
+import com.ekeitho.bubble.views.ScrollingBubble
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.toPersistentList
+import kotlinx.collections.immutable.toPersistentMap
 
 val alphabetItemSize = 24.dp
+val alphabetCharList = "abcdefghijklmnopqrstuvwxyz".map { it }.toPersistentList()
 
 internal fun Float.getIndexOfCharBasedOnYPosition(
-    alphabetHeightInPixels: Float,
+  alphabetHeightInPixels: Float,
 ): Char {
-    val alphabetCharList = "abcdefghijklmnopqrstuvwxyz".map { it }
 
-    var index = ((this) / alphabetHeightInPixels).toInt()
-    index = when {
-        index > 25 -> 25
-        index < 0 -> 0
-        else -> index
-    }
-    return alphabetCharList[index]
+  var index = ((this) / alphabetHeightInPixels).toInt()
+  index = when {
+    index > 25 -> 25
+    index < 0 -> 0
+    else -> index
+  }
+  return alphabetCharList[index]
 }
 
 class ScrollingBubbleActivity : ComponentActivity() {
-    private val names =
-        "Aaren, Aarika, Abagael, Bab, Babara, Babb, Cacilia, Cacilie, Cahra, Dacey, Dacia, Dacie, Eachelle, Eada, Eadie, Fae, Faina, Faith, Gabbey, Gabbi, Gabbie, Hadria, Hailee, Haily, Ianthe, Ibbie, Ibby, Jacenta, Jacinda, Jacinta, Kacey, Kacie, Kacy, La Verne, Lacee, Lacey, Mab, Mabel, Mabelle, Nada, Nadean, Nadeen, Octavia, Odele, Odelia, Page, Paige, Paloma, Queenie, Quentin, Querida, Rachael, Rachel, Rachele, Saba, Sabina, Sabine, Tabatha, Tabbatha, Tabbi, Ula, Ulla, Ulrica, Val, Valaree, Valaria, Wallie, Wallis, Walliw, Xaviera, Xena, Xenia, Yalonda, Yasmeen, Yasmin, Zabrina, Zahara, Zandra"
-    private val contacts = names.split(", ").map { Contact(it) }
+  private val names =
+    "Aaren, Aarika, Abagael, Bab, Babara, Babb, Cacilia, Cacilie, Cahra, Dacey, Dacia, " +
+      "Dacie, Eachelle, Eada, Eadie, Fae, Faina, Faith, Gabbey, Gabbi, Gabbie, Hadria, " +
+      "Hailee, Haily, Ianthe, Ibbie, Ibby, Jacenta, Jacinda, Jacinta, Kacey, Kacie, Kacy, " +
+      "La Verne, Lacee, Lacey, Mab, Mabel, Mabelle, Nada, Nadean, Nadeen, Octavia, Odele, " +
+      "Odelia, Page, Paige, Paloma, Queenie, Quentin, Querida, Rachael, Rachel, Rachele, " +
+      "Saba, Sabina, Sabine, Tabatha, Tabbatha, Tabbi, Ula, Ulla, Ulrica, Val, Valaree, " +
+      "Valaria, Wallie, Wallis, Walliw, Xaviera, Xena, Xenia, Yalonda, Yasmeen, Yasmin, " +
+      "Zabrina, Zahara, Zandra"
+  private val contacts = names.split(", ").map { Contact(it) }.toPersistentList()
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            val context = LocalDensity.current
-            val alphabetHeightInPixels = remember { with(context) { alphabetItemSize.toPx() } }
-            var alphabetRelativeDragYOffset: Float? by remember { mutableStateOf(null) }
-            var alphabetDistanceFromTopOfScreen: Float by remember { mutableStateOf(0F) }
-            ScrollingBubbleTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BoxWithConstraints {
-                        ContactListWithScroller(
-                            contacts = contacts,
-                            onAlphabetListDrag = { relativeDragYOffset, containerDistance ->
-                                alphabetRelativeDragYOffset = relativeDragYOffset
-                                alphabetDistanceFromTopOfScreen = containerDistance
-                            }
-                        )
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      val context = LocalDensity.current
+      val alphabetHeightInPixels = remember { with(context) { alphabetItemSize.toPx() } }
+      var alphabetRelativeDragYOffset: Float? by remember { mutableStateOf(null) }
+      var alphabetDistanceFromTopOfScreen: Float by remember { mutableStateOf(0F) }
+      ScrollingBubbleTheme {
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          BoxWithConstraints {
+            ContactListWithScroller(
+              contacts = contacts,
+              onAlphabetListDrag = { relativeDragYOffset, containerDistance ->
+                alphabetRelativeDragYOffset = relativeDragYOffset
+                alphabetDistanceFromTopOfScreen = containerDistance
+              }
+            )
 
-                        val yOffset = alphabetRelativeDragYOffset
-                        if (yOffset != null) {
-                            ScrollingBubble(
-                                boxConstraintMaxWidth = this.maxWidth,
-                                bubbleOffsetYFloat = yOffset + alphabetDistanceFromTopOfScreen,
-                                currAlphabetScrolledOn = yOffset.getIndexOfCharBasedOnYPosition(
-                                    alphabetHeightInPixels = alphabetHeightInPixels,
-                                ),
-                            )
-                        }
-                    }
-                }
+            val yOffset = alphabetRelativeDragYOffset
+            if (yOffset != null) {
+              ScrollingBubble(
+                boxConstraintMaxWidth = this.maxWidth,
+                bubbleOffsetYFloat = yOffset + alphabetDistanceFromTopOfScreen,
+                currAlphabetScrolledOn = yOffset.getIndexOfCharBasedOnYPosition(
+                  alphabetHeightInPixels = alphabetHeightInPixels,
+                ),
+              )
             }
+          }
         }
+      }
     }
+  }
 }
 
-
-fun List<Contact>.getFirstUniqueSeenCharIndex(): Map<Char, Int> {
-    val firstLetterIndexes = mutableMapOf<Char, Int>()
-    this
-        .map { it.fullName.lowercase().first() }
-        .forEachIndexed { index, char ->
-            if (!firstLetterIndexes.contains(char)) {
-                firstLetterIndexes[char] = index
-            }
-            // else don't care about letters that don't exist
-        }
-    return firstLetterIndexes
+fun List<Contact>.getFirstUniqueSeenCharIndex(): ImmutableMap<Char, Int> {
+  val firstLetterIndexes = mutableMapOf<Char, Int>()
+  this
+    .map { it.fullName.lowercase().first() }
+    .forEachIndexed { index, char ->
+      if (!firstLetterIndexes.contains(char)) {
+        firstLetterIndexes[char] = index
+      }
+      // else don't care about letters that don't exist
+    }
+  return firstLetterIndexes.toPersistentMap()
 }
--- app/src/main/java/com/ekeitho/bubble/views/ContactListWithScroller.kt
+++ app/src/main/java/com/ekeitho/bubble/views/ContactListWithScroller.kt
@@ -1,16 +1,32 @@
 package com.ekeitho.bubble.views
 
 import androidx.compose.foundation.gestures.detectVerticalDragGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -21,148 +37,148 @@ import com.ekeitho.bubble.alphabetItemSize
 import com.ekeitho.bubble.domain.Contact
 import com.ekeitho.bubble.getFirstUniqueSeenCharIndex
 import com.ekeitho.bubble.getIndexOfCharBasedOnYPosition
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.ImmutableMap
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ContactListWithScroller(
-    contacts: List<Contact>,
-    onAlphabetListDrag: (Float?, Float) -> Unit,
+  contacts: ImmutableList<Contact>,
+  onAlphabetListDrag: (Float?, Float) -> Unit,
 ) {
-    val mapOfFirstLetterIndex = remember(contacts) { contacts.getFirstUniqueSeenCharIndex() }
-    val alphabetHeightInPixels = with(LocalDensity.current) { alphabetItemSize.toPx() }
-    val lazyListState = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
+  val mapOfFirstLetterIndex: ImmutableMap<Char, Int> =
+    remember(contacts) { contacts.getFirstUniqueSeenCharIndex() }
+  val alphabetHeightInPixels: Float =
+    with(LocalDensity.current) { alphabetItemSize.toPx() }
+  val lazyListState = rememberLazyListState()
+  val coroutineScope = rememberCoroutineScope()
 
-    Row(
-        modifier = Modifier.fillMaxSize(),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        ContactList(
-            Modifier
-                .fillMaxHeight()
-                .weight(1F),
-            contacts,
-            lazyListState,
-            mapOfFirstLetterIndex
-        )
+  Row(
+    modifier = Modifier.fillMaxSize(),
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
+    ContactList(
+      Modifier
+        .fillMaxHeight()
+        .weight(1F),
+      contacts,
+      lazyListState,
+      mapOfFirstLetterIndex
+    )
 
-        AlphabetScroller(
-            onAlphabetListDrag = { relativeDragYOffset, containerDistanceFromTopOfScreen ->
-                onAlphabetListDrag(relativeDragYOffset, containerDistanceFromTopOfScreen)
-                coroutineScope.launch {
-                    // null case can happen if we go through list
-                    // and we don't have a name that starts with I
-                    val indexOfChar = relativeDragYOffset?.getIndexOfCharBasedOnYPosition(
-                        alphabetHeightInPixels = alphabetHeightInPixels,
-                    )
-                    mapOfFirstLetterIndex[indexOfChar]?.let {
-                        lazyListState.scrollToItem(it)
-                    }
-                }
-            },
-        )
-    }
+    AlphabetScroller(
+      onAlphabetListDrag = { relativeDragYOffset, containerDistanceFromTopOfScreen ->
+        onAlphabetListDrag(relativeDragYOffset, containerDistanceFromTopOfScreen)
+        coroutineScope.launch {
+          // null case can happen if we go through list
+          // and we don't have a name that starts with I
+          val indexOfChar = relativeDragYOffset?.getIndexOfCharBasedOnYPosition(
+            alphabetHeightInPixels = alphabetHeightInPixels,
+          )
+          mapOfFirstLetterIndex[indexOfChar]?.let {
+            lazyListState.scrollToItem(it)
+          }
+        }
+      },
+    )
+  }
 }
 
 @Composable
 fun RowScope.ContactList(
-    modifier: Modifier,
-    contacts: List<Contact>,
-    lazyListState: LazyListState,
-    firstLetterIndexes: Map<Char, Int>,
+  modifier: Modifier,
+  contacts: ImmutableList<Contact>,
+  lazyListState: LazyListState,
+  firstLetterIndexes: Map<Char, Int>,
 ) {
-    LazyColumn(
-        modifier = modifier,
-        state = lazyListState,
-    ) {
-        itemsIndexed(contacts) { index, contact ->
-            ContactItem(
-                contact = contact,
-                isAlphabeticallyFirstInCharGroup =
-                firstLetterIndexes[contact.fullName.lowercase().first()] == index,
-            )
-        }
+  LazyColumn(
+    modifier = modifier,
+    state = lazyListState,
+  ) {
+    itemsIndexed(contacts) { index, contact ->
+      ContactItem(
+        contact = contact,
+        isAlphabeticallyFirstInCharGroup =
+        firstLetterIndexes[contact.fullName.lowercase().first()] == index,
+      )
     }
+  }
 }
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactItem(
-    contact: Contact,
-    isAlphabeticallyFirstInCharGroup: Boolean
+  contact: Contact,
+  isAlphabeticallyFirstInCharGroup: Boolean
 ) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Box(
-            modifier = Modifier.width(48.dp),
-            contentAlignment = Alignment.Center,
-        ) {
-            if (isAlphabeticallyFirstInCharGroup) {
-                Text(
-                    text = contact.fullName.first().toString(),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
-        }
-
-        Surface(
-            shape = CircleShape,
-            modifier = Modifier.size(32.dp),
-            color = MaterialTheme.colorScheme.secondary
-        ) {
-            Box(contentAlignment = Alignment.Center) {
-                Text(
-                    text = contact.fullName.first().toString(),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-            }
-        }
-
+  Row(verticalAlignment = Alignment.CenterVertically) {
+    Box(
+      modifier = Modifier.width(48.dp),
+      contentAlignment = Alignment.Center,
+    ) {
+      if (isAlphabeticallyFirstInCharGroup) {
         Text(
-            modifier = Modifier.padding(16.dp),
-            text = contact.fullName,
-            style = MaterialTheme.typography.titleLarge,
+          text = contact.fullName.first().toString(),
+          style = MaterialTheme.typography.bodyLarge
         )
+      }
     }
+
+    Surface(
+      shape = CircleShape,
+      modifier = Modifier.size(32.dp),
+      color = MaterialTheme.colorScheme.secondary
+    ) {
+      Box(contentAlignment = Alignment.Center) {
+        Text(
+          text = contact.fullName.first().toString(),
+          style = MaterialTheme.typography.bodyLarge
+        )
+      }
+    }
+
+    Text(
+      modifier = Modifier.padding(16.dp),
+      text = contact.fullName,
+      style = MaterialTheme.typography.titleLarge,
+    )
+  }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun AlphabetScroller(
-    onAlphabetListDrag: (relativeDragYOffset: Float?, distanceFromTopOfScreen: Float) -> Unit,
+  onAlphabetListDrag: (relativeDragYOffset: Float?, distanceFromTopOfScreen: Float) -> Unit,
 ) {
-    val alphabetCharList = "abcdefghijklmnopqrstuvwxyz".map { it }
-    var distanceFromTopOfScreen by remember { mutableStateOf(0F) }
+  val alphabetCharList = "abcdefghijklmnopqrstuvwxyz".map { it }
+  var distanceFromTopOfScreen by remember { mutableStateOf(0F) }
 
-    Column(
-        modifier = Modifier
-            .width(16.dp)
-            .onGloballyPositioned {
-                distanceFromTopOfScreen = it.positionInRoot().y
-            }
-            .pointerInput(alphabetCharList) {
-                detectVerticalDragGestures(
-                    onDragStart = {
-                        onAlphabetListDrag(it.y, distanceFromTopOfScreen)
-                    },
-                    onDragEnd = {
-                        onAlphabetListDrag(null, distanceFromTopOfScreen)
-                    }
-                ) { change, _ ->
-                    onAlphabetListDrag(
-                        change.position.y,
-                        distanceFromTopOfScreen
-                    )
-                }
-            },
-        verticalArrangement = Arrangement.Center,
-    ) {
-        for (i in alphabetCharList) {
-            Text(
-                modifier = Modifier.height(alphabetItemSize),
-                text = i.toString(),
-            )
+  Column(
+    modifier = Modifier
+      .width(16.dp)
+      .onGloballyPositioned {
+        distanceFromTopOfScreen = it.positionInRoot().y
+      }
+      .pointerInput(alphabetCharList) {
+        detectVerticalDragGestures(
+          onDragStart = {
+            onAlphabetListDrag(it.y, distanceFromTopOfScreen)
+          },
+          onDragEnd = {
+            onAlphabetListDrag(null, distanceFromTopOfScreen)
+          }
+        ) { change, _ ->
+          onAlphabetListDrag(
+            change.position.y,
+            distanceFromTopOfScreen
+          )
         }
+      },
+    verticalArrangement = Arrangement.Center,
+  ) {
+    for (i in alphabetCharList) {
+      Text(
+        modifier = Modifier.height(alphabetItemSize),
+        text = i.toString(),
+      )
     }
+  }
 }
